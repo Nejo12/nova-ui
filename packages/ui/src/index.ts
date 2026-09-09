acf62d3654daef2859eaba9aba286bde7c05e865
Olaniyi Gabriel Aborisade <42632188+Nejo12@users.noreply.github.com>
2026-09-09 00:59:05 -0700

    style(ui): apply Prettier formatting to Dialog export

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,12 +1,7 @@
 export type { NovaResolvedTheme } from '@nova/design-tokens';
 
 export { Dialog } from './dialog/dialog';
-export type {
-  DialogAction,
-  DialogProps,
-  DialogSize,
-  DialogType,
-} from './dialog/dialog';
+export type { DialogAction, DialogProps, DialogSize, DialogType } from './dialog/dialog';
 
 export { InlineAlert } from './inline-alert/inline-alert';
 export type {
